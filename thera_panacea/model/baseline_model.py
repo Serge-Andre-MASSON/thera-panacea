af072683fdc289c9b94f372e2e22365c7ca377b5
Serge <serge.andre.masson@gmail.com>
2023-07-17 17:55:28 +0200

    implement the baseline model

---
+++ thera_panacea/model/baseline_model.py
@@ -0,0 +1,23 @@
+import torch
+from torch import nn
+import torch.nn.functional as F
+
+
+class BaselineModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(2704, 256)
+        self.fc2 = nn.Linear(256, 32)
+        self.fc3 = nn.Linear(32, 2)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = torch.flatten(x, 1)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
